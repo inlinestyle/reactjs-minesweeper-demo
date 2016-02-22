658972fad66d16154c01b5c0f9a367daeafe9e38
byels <byelsey1@gmail.com>
2016-02-22 00:21:19 -0500

    Adding CSS to build

--- app.jsx
+++ app.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import MineSweeper from './components/MineSweeper.jsx';
+import './static/main.css'
 
 ReactDom.render(<MineSweeper/>, document.getElementById('mineSweeper'))
--- webpack.config.js
+++ webpack.config.js
@@ -10,12 +10,15 @@ module.exports = {
     module: {
         loaders: [
             {
-                test: /.jsx?$/,
+                test: /\.jsx?$/,
                 loader: 'babel-loader',
                 exclude: /node_modules/,
                 query: {
                     presets: ['es2015', 'react']
                 }
+            }, {
+                test: /\.css$/,
+                loader: 'style!css'
             }
         ]
     }
